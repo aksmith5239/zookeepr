8653e970dfe96cb43b87dea20f62b4f3efe8e078
aksmith5239 <aklobby@gmail.com>
2020-10-05 16:10:45 -0600

    updated animals by id

--- server.js
+++ server.js
@@ -38,6 +38,10 @@ function filterByQuery(query, animalsArray) {
     }
     return filteredResults;
 }
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+  }
 //res = response
 //req = request
 app.get('/api/animals', (req,res) => {
@@ -46,6 +50,14 @@ app.get('/api/animals', (req,res) => {
         results = filterByQuery(req.query, results);
     }
     res.json(results);
+});
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
 });
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
