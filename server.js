a8de3961b6c5a5c842e59892c7168f04f55a5b17
aksmith5239 <aklobby@gmail.com>
2020-10-07 13:58:43 -0600

    added front end functionality for animals

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
-
+app.use(express.static('public'));
 const { animals } = require('./data/animals');
 
 function filterByQuery(query, animalsArray) {
@@ -109,7 +109,21 @@ app.post('/api/animals', (req, res) => {
     }
   });
 
+  // front end routes
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
 
+//port lisener
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
